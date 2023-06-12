a0f5d6ed6b0ac93c1b7c4d4549b0c254ed791662
Higor Gustavo Martins Braga <98365126+HigorBraga@users.noreply.github.com>
2023-06-12 00:16:47 -0300

    Criando um menu hamburguer responsivo

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function abrirMenu() {
+    let menu = window.document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
