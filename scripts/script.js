ecdf06b94c30ca51a18ada9041bf5f7479e45741
HigorBraga <higgorbraga123@gmail.com>
2023-06-12 00:55:17 -0300

    Projeto menu responsivo
    Nesta atualização contém a finalização do projeto menu responsivo fazendo com que o mesmo se encaixa em varios tamanhos de tela.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+
+
 function abrirMenu() {
     let menu = window.document.getElementById('menu');
     if (menu.style.display === 'block') {
